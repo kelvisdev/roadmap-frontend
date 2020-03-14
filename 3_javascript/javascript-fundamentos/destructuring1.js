b72328b248628b5f7bd87c20b102bd81bb39a1d8
Kelvis Borges <kelvisbcc@gmail.com>
2020-03-14 13:15:04 -0300

    learning javascript fundamentals, assignment, notation point, destructuring

---
+++ 3_javascript/javascript-fundamentos/destructuring1.js
@@ -0,0 +1,27 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+console.log(pessoa);
+
+const { nome, idade } = pessoa
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa
+console.log(n, 1);
+
+const { sobrenome, bemHumorada = true } = pessoa
+console.log(sobrenome, bemHumorada);
+
+const { endereco } = pessoa
+console.log(endereco);
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep);
+
