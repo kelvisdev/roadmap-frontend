d7d629d2dd13a0ae0887105e141ea76da3130f48
Kelvis Borges <kelvisbcc@gmail.com>
2020-03-21 12:00:44 -0300

    learning javascript fundamentals, object orientation

---
+++ 3_javascript/javascript-fundamentos/objeto/oo.js
@@ -0,0 +1,22 @@
+// Código não executável!!!
+
+// Procedural, funcoes que manipula dados
+processamento(valor1, valor2, valor3)
+
+// OO, dados manipula funcoes
+objeto = {
+    valor1,
+    valor2, 
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança
+// 4. polimorfismo
